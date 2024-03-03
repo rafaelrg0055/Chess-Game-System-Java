e780f5f1ec33152ce311cc2f8dda0414e6d6aec5
rafael0055 <rafaelrg_5@hotmail.com>
2024-03-03 17:25:21 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -28,6 +32,22 @@ public class UI {
 		
 		
 	
+		
+		
+		
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0); 
+			int  row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");   //InputMismatchException é um erro de entrada de dados do Java
+		}
+	}
+		
+		
 	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i=0; i<pieces.length; i++) {
