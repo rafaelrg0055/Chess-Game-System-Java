11a39308c502080d8b5be7cc62e9b6d35a395d8f
rafael0055 <rafaelrg_5@hotmail.com>
2024-03-09 16:02:55 -0300

    Queen

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -51,7 +51,7 @@ public class Pawn extends ChessPiece{
 			}
 			//pode avançar 2 casas
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
